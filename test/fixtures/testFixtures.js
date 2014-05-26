e3af3e52b3e600129240e329c7016635b0733975
sashless <saweniger@gmail.com>
2014-05-27 00:46:49 +0200

    added fixture test

---
+++ test/fixtures/testFixtures.js
@@ -0,0 +1,20 @@
+var fixtures = JSON.parse(unescape(casper.cli.options["fixtures"]));
+
+var run = function(fixture, index){
+    casper.test.begin('Fixture test #' + index, 3, function suite(test) {
+        casper.start('test/fixtures/basicSite.html', function() {
+            test.assertTitle(fixture.title);
+            test.assertSelectorHasText('h1', fixture.h1);
+            test.assertSelectorHasText('p', fixture.p);
+        });
+
+        casper.run(function() {
+            test.done();
+        });
+
+    });
+};
+
+for(var i = 0; i < fixtures.length; i++){
+    run(fixtures[o], i);
+}
